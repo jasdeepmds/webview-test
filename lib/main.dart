9accdd6120df178052c603c002c2785edd1748f0
Jasdeep Singh <jasdeep@metadesignsolutions.co.uk>
2020-12-17 12:55:02 +0530

    - removed clearCache from webview

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
             javaScriptEnabled: true,
             supportZoom: false,
             // debuggingEnabled: true,
-            clearCache: true,
+            // clearCache: true,
           ),
         ),
         onWebViewCreated: (InAppWebViewController _controller) async {
